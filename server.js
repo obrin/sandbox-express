59ae9b52f15c6ceabe0cdd8c72f773c123e15ff3
obrin <jordanyong23@gmail.com>
2021-06-06 21:08:34 +0200

    Add ping endpoint

--- server.js
+++ server.js
@@ -7,4 +7,8 @@ app.get('/', (request, response) => {
   response.send('Hello World');
 })
 
+app.get('/ping', (request, response) => {
+  response.send('pong');
+})
+
 app.listen(port, () => console.log('listening on port:', port))
